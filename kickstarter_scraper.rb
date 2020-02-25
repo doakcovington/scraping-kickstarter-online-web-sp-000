b3bd5e303e33e129177abfab07735dc01da32ad1
Doak Covington <doakcovington@protonmail.com>
2020-02-25 02:28:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,7 +15,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
-  binding.pry
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  projects
 end
 
 create_project_hash
