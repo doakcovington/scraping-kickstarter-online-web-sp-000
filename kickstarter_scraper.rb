264f39531c1d56167538f33e46e55b26fadd91c6
Doak Covington <doakcovington@protonmail.com>
2020-02-25 02:13:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,7 @@ def create_project_hash
   # write your code here
   #projects: kickestarter.css("li.project.gtid_4")
   #project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
